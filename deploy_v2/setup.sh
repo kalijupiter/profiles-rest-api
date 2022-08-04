bb32c4620f93012f3432ce344c2d36a0e499bbe0
kalijupiter <110563457+kalijupiter@users.noreply.github.com>
2022-08-04 12:23:40 -0400

    Update setup.sh

--- deploy_v2/setup.sh
+++ deploy_v2/setup.sh
@@ -13,7 +13,7 @@ apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx g
 
 # Create project directory
 # mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+# git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Create virtual environment
 mkdir -p $PROJECT_BASE_PATH/env
